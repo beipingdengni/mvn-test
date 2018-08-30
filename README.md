8ec56b9c67a2eb2c8ad7f32b9fe97c429fd1aba3
beipingdengni <1271727449@qq.com>
2018-08-30 16:33:16 +0800

    添加注解

--- README.md
+++ README.md
@@ -15,3 +15,43 @@
 
 ```
 
+#### 动态代理接口 无实现类使用
+```java
+    public interface Person {
+        void say(String ok);
+    }
+    
+    public class MainTest{
+        public static void main(String[] args) {
+            Person person = newProxy(Person.class);
+            
+                    person.say("hello world");
+        }
+    }
+    
+```
+#### 代理类实现
+```java
+    public static class FacadeProxy implements InvocationHandler {
+
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("接口方法调用开始");
+            //执行方法
+            System.out.println("method toGenericString:" + method.toGenericString());
+            System.out.println("method name:" + method.getName());
+            System.out.println("method args:" + (String) args[0]);
+            Object invoke = method.invoke(args);
+            System.out.println("接口方法调用结束");
+            return invoke;
+        }
+        
+        public static <T> T newProxy(Class<T> mapperInterface) {
+                ClassLoader classLoader = mapperInterface.getClassLoader();
+                Class<?>[] classes = new Class[]{mapperInterface};
+                FacadeProxy facadeProxy = new FacadeProxy();
+                return (T) Proxy.newProxyInstance(classLoader, classes, facadeProxy);
+            }
+    }
+
+```
+
--- spring-quartz/src/main/java/com/tian/spring/quartz/ScheduleFactorySpringMainTest.java
+++ spring-quartz/src/main/java/com/tian/spring/quartz/ScheduleFactorySpringMainTest.java
@@ -1,11 +1,21 @@
 package com.tian.spring.quartz;
 
+import com.tian.spring.quartz.annotion.CustomQuartz;
+import com.tian.spring.quartz.annotion.QuartzCron;
 import org.quartz.*;
+import org.quartz.impl.JobExecutionContextImpl;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.Date;
 import java.util.Properties;
 
@@ -18,17 +28,52 @@ import java.util.Properties;
  */
 public class ScheduleFactorySpringMainTest implements Job {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SchedulerException, IOException {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-schedule-factory.xml");
-        SchedulerFactoryBean schedulerFactoryBean = context.getBean("schedulerFactoryBean", SchedulerFactoryBean.class);
-        Scheduler scheduler = schedulerFactoryBean.getObject();
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring-schedule-factory.xml");
+//        SchedulerFactoryBean schedulerFactoryBean = context.getBean("schedulerFactoryBean", SchedulerFactoryBean.class);
+//        Scheduler scheduler = schedulerFactoryBean.getObject();
+//
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        System.out.println("======> end <======");
 
+        String groupName = "ScheduleFactorySpringMainTest---group";
+        String trigerName = "ScheduleFactorySpringMainTest---trigger";
+        String jobName = "ScheduleFactorySpringMainTest---job";
+
+        JobKey jobKey = JobKey.jobKey(jobName, groupName);
+        TriggerKey triggerKey = TriggerKey.triggerKey(trigerName, groupName);
+
+        String cron = "0/10 * * * * ?";
+
+        JobDetail jobDetail = JobBuilder.newJob(ScheduleFactorySpringMainTest.class).withIdentity(jobKey).build();
+        CronTrigger cronTrigger = TriggerBuilder.newTrigger().withIdentity(triggerKey).startNow()
+                .withSchedule(CronScheduleBuilder.cronSchedule(cron).withMisfireHandlingInstructionDoNothing())
+                .build();
+
+        boolean b = scheduler.checkExists(triggerKey);
+        if (b) {
+            scheduler.pauseTrigger(triggerKey);
+            scheduler.unscheduleJob(triggerKey);
+        }
+        boolean a = scheduler.checkExists(jobKey);
+        if (a) {
+            scheduler.deleteJob(jobKey);
+        }
+
+        scheduler.scheduleJob(jobDetail, cronTrigger);
+        System.out.println("======> end <======");
 
     }
+
 
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println(" 启动开始执行job ===> " + System.currentTimeMillis());
+        System.out.println("start ====> 启动开始执行job ===> " + System.currentTimeMillis());
+
+
+        System.out.println("end ===> 启动开始执行job ===> " + System.currentTimeMillis());
     }
+
 
 }
