741385fabb7bdcfebce8efb80db7d03654c8de8a
beipingdengni <1271727449@qq.com>
2018-09-05 14:54:40 +0800

    add config of log4j2 and spring-mybatis file

---
+++ spring-mybatis/src/main/java/com/tian/spring/myabtis/MainTest.java
@@ -0,0 +1,11 @@
+package com.tian.spring.myabtis;
+
+/**
+ * @author tianbeiping
+ * @Title: MainTest
+ * @ProjectName mvn-test
+ * @Description:
+ * @date 18/9/5下午2:53
+ */
+public class MainTest {
+}
