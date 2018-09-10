9e22b7805061a6c3847a09c0935c14f98ad9d142
beipingdengni <1271727449@qq.com>
2018-09-10 18:45:47 +0800

    jpa

--- spring-jpa/src/main/java/com/tian/spring/jpa/repository/TPersonRepository.java
+++ spring-jpa/src/main/java/com/tian/spring/jpa/repository/TPersonRepository.java
@@ -1,11 +1,12 @@
 package com.tian.spring.jpa.repository;
 
 import com.tian.spring.jpa.jpa.TPersonEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -26,4 +27,13 @@ public interface TPersonRepository extends JpaRepository<TPersonEntity, Integer>
     @Modifying
     @Query(value = "delete * from t_person where id=:id", nativeQuery = true)
     int deleteByMyId(@Param("id") Integer id);
+
+    @Query(value = "SELECT * FROM t_person WHERE age = ?1",
+            countQuery = "SELECT count(*) FROM t_person WHERE age = ?1",
+            nativeQuery = true)
+    Page<TPersonEntity> findByLastname(String lastname, Pageable pageable);
+
+
+    @Query(value = "SELECT * FROM t_person WHERE id > ?1", nativeQuery = true)
+    Page<TPersonEntity> findTPersonById(int id, Pageable pageable);
 }
--- spring-jpa/src/main/java/com/tian/spring/jpa/services/PersonService.java
+++ spring-jpa/src/main/java/com/tian/spring/jpa/services/PersonService.java
@@ -1,11 +1,17 @@
 package com.tian.spring.jpa.services;
+import java.util.ArrayList;
 
 import com.tian.spring.jpa.jpa.TPersonEntity;
 import com.tian.spring.jpa.repository.TPersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -26,6 +32,21 @@ public class PersonService {
 
         List<TPersonEntity> all = tPersonRepository.findAll();
         List<TPersonEntity> byId = tPersonRepository.getByMyId(2);
+
+        Pageable pageable=new QPageRequest(1,3);
+
+        Page<TPersonEntity> tPersonById = tPersonRepository.findTPersonById(0, pageable);
+
+        Iterator<TPersonEntity> iterator = tPersonById.iterator();
+        while (iterator.hasNext()) {
+            TPersonEntity next = iterator.next();
+
+            System.out.println(next);
+        }
+
+
+        System.out.println("===========================");
+
         return all;
     }
 
