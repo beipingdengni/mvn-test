b9d5c00dcef6658ea9b9c38d8e9afee34f16603c
beipingdengni <1271727449@qq.com>
2018-09-12 16:23:13 +0800

    push

---
+++ spring-analysis/src/test/java/com/tian/spring/analysis/AnnotatedGenericBeanDefinitionMainTestTest.java
@@ -0,0 +1,20 @@
+package com.tian.spring.analysis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author tianbeiping
+ * @Title: AnnotatedGenericBeanDefinitionMainTestTest
+ * @ProjectName mvn-test
+ * @Description:
+ * @date 18/9/12下午4:22
+ */
+public class AnnotatedGenericBeanDefinitionMainTestTest {
+
+    @Test
+    public void main() {
+
+    }
+}
