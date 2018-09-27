9ebab755ff0e318c5c00058f59603ba22b574c37
beipingdengni <1271727449@qq.com>
2018-09-27 15:01:59 +0800

    oush

--- spring-common/src/main/java/com/tian/spring/common/date/DatetimeUtil.java
+++ spring-common/src/main/java/com/tian/spring/common/date/DatetimeUtil.java
@@ -1,9 +1,9 @@
 package com.tian.spring.common.date;
 
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.ZoneId;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
 
 /**
  * @author tianbeiping
@@ -28,7 +28,9 @@ public class DatetimeUtil {
     private static final DateTimeFormatter fmt_HHmm_noSplit = DateTimeFormatter.ofPattern("HHmm");
     private static final DateTimeFormatter fmt_YYYYMMDDHHMM = DateTimeFormatter.ofPattern("yyyyMMdd HH:mm");
 
-    private static final LocalDateTime dateTime = ZonedDateTime.now(ZoneId.systemDefault()).toLocalDateTime();
+    public static final LocalDateTime dateTime = ZonedDateTime.now(ZoneId.systemDefault()).toLocalDateTime();
+
+    //TimeZone.getAvailableIDs()
 
     public static String getNowSss() {
         return fmt_yyyyMMddHHmmssSSS.format(dateTime);
@@ -48,6 +50,27 @@ public class DatetimeUtil {
 
     public static String getTime() {
         return fmt_HHmmss.format(dateTime);
+    }
+
+    public static Date parse(String dateTime) {
+        LocalDateTime parse = LocalDateTime.parse(dateTime, fmt_yyyyMMddHHmmss);
+        return Date.from(parse.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static Date parse(String dateTime, String pattern) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
+        LocalDateTime parse = LocalDateTime.parse(dateTime, dateTimeFormatter);
+        return Date.from(parse.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static Date LocalDateToDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static LocalDateTime DateToLocalDate(Date dateTime) {
+        Instant instant = dateTime.toInstant();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        return localDateTime;
     }
 
 
