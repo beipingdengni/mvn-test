678902fe064ffe7f7bbd485a456337ccb17ed260
beipingdengni <1271727449@qq.com>
2018-10-30 10:35:04 +0800

    work

--- spring-jvm/src/main/java/com/tian/spring/jvm/YanMainTest.java
+++ spring-jvm/src/main/java/com/tian/spring/jvm/YanMainTest.java
@@ -1,6 +1,17 @@
 package com.tian.spring.jvm;
 
+import com.google.common.collect.Lists;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -26,7 +37,38 @@ public class YanMainTest {
         int[] stringsw = Arrays.asList(shopId.split(",")).stream().mapToInt(Integer::valueOf).toArray();
         boolean b = Arrays.asList(shopId.split(",")).stream().map(Integer::valueOf).anyMatch(e -> 1212 == e);
 
-        
+        LocalDateTime localDateTime = LocalDateTime.now(ZoneId.systemDefault());
+        LocalDateTime localDateTime1 = localDateTime.minusDays(3);
+        String format = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
+        System.out.println(format);
+
+
+        System.out.println(8 & 4);
+        System.out.println(Integer.toBinaryString(1));
+        System.out.println(Integer.toBinaryString(2));
+        System.out.println(Integer.toBinaryString(4));
+        System.out.println(Integer.toBinaryString(8));
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+
+        List<Discount> ls = Lists.newArrayList();
+        ls.add(new Discount(1));
+        ls.add(new Discount(13));
+        ls.add(new Discount(1));
+        ls.add(new Discount(122));
+
+        Collections.sort(ls, Comparator.comparingInt(Discount::getWait));
+
+        System.out.println(ls);
+
+
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    public static class Discount {
+        private int wait;
     }
 }
